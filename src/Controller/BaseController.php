f1cf1e2f82088df98b8b9e401791a93c17b35bd0
Picpepper <clementdesj@hotmail.fr>
2025-03-24 16:40:08 +0100

    Liste des utilisateurs

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
+use Doctrine\ORM\Exception\RepositoryException;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,9 +11,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class BaseController extends AbstractController
 {
     #[Route('/', name: 'app_base')]
-    public function index(): Response
+    public function index(UserRepository $userRepository): Response
     {
+        $users = $userRepository->findAll();
+
         return $this->render('base.html.twig', [
+            'users' => $users
         ]);
     }
 }
